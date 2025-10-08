b44afcdb5adb7af2fb545fd525071e66523051f1
msfaraja <msfaraja@asystresources.com>
2025-10-08 12:13:41 +0200

    BUILD

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
