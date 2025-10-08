0772b02a5df7e1a6f779ad49c1c3cef578a737bf
msfaraja <msfaraja@asystresources.com>
2025-10-08 10:50:50 +0200

    serveur

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
