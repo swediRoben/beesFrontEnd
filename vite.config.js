3b0e6e9567aa6782b6326ae76ae765a54a73970a
swediRoben <farajampenda43@gmail.com>
2025-10-11 18:18:37 +0200

    vite change

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/',
-    server: {
-        port: 5000 // ou une autre valeur
-    }
+    base: '/'
 })
