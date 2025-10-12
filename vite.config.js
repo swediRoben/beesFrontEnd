7382a6ae4b8e717bc00804198e6b1a3867bc7a50
swediRoben <farajampenda43@gmail.com>
2025-10-12 13:52:18 +0200

    MON DOMAINE

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/'
+    base: '/',
+    preview: {
+      allowedHosts: [
+        'beesfrontend-production.up.railway.app' // 👈 ajoute ici ton domaine Railway
+      ]
+    }
 })
