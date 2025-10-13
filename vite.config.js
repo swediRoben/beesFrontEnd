a3fc259b69c4a09b4194b660f89fdd8f34a0fd35
swediRoben <farajampenda43@gmail.com>
2025-10-13 14:10:30 +0200

    vite js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-    plugins: [react()],
-    base: '/',
-    preview: {
-      allowedHosts: [
-        'beesfrontend-production.up.railway.app' // 👈 ajoute ici ton domaine Railway
-      ]
-    }
+  plugins: [react()],
+  base: '/',
+  preview: {
+    allowedHosts: [
+      'beesfrontend-production.up.railway.app', // domaine Railway
+      'www.beesasbl.org' // ton domaine custom ou celui vers lequel tu fais une requête
+    ]
+  }
 })
